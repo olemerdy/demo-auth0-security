ef9dae41cac1d54499c25e9801a604efad912005
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-01-09 15:00:38 +0100

    Card

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { YouTubePlayer } from '@angular/youtube-player';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, YouTubePlayer],
+  imports: [MatButtonModule, MatCardModule, RouterOutlet, YouTubePlayer],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'frontend-angular';
