cd63d3d640739415180b33e554e3515ac8292c58
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-01-09 12:01:33 +0100

    Google Maps

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 import { RouterOutlet } from '@angular/router';
 import { YouTubePlayer } from '@angular/youtube-player';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,7 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatCardModule, RouterOutlet, YouTubePlayer],
+  imports: [GoogleMap, MatButtonModule, MatCardModule, RouterOutlet, YouTubePlayer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
