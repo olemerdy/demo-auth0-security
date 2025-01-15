ecb05554ab317753f30cacfebf5c70ffb736b4ab
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-01-15 22:32:43 +0100

    H2 and some implementation

--- backend-spring/src/main/java/org/lafeuille/demo/web/CalendarController.java
+++ backend-spring/src/main/java/org/lafeuille/demo/web/CalendarController.java
@@ -1,9 +1,28 @@
 package org.lafeuille.demo.web;
 
+import org.lafeuille.demo.data.Event;
+import org.lafeuille.demo.services.CalendarService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/calendar")
 public class CalendarController {
+
+    private final CalendarService calendarService;
+
+    public CalendarController(CalendarService calendarService) {
+        this.calendarService = calendarService;
+    }
+
+    @GetMapping("{id}/events")
+    Page<Event> readCalendarEvents(@PathVariable UUID id, Pageable pageable) {
+        return calendarService.getCalendarEvents(id, pageable);
+    }
 }
--- backend-spring/src/test/java/org/lafeuille/demo/TestApplication.java
+++ backend-spring/src/test/java/org/lafeuille/demo/TestApplication.java
@@ -2,22 +2,9 @@ package org.lafeuille.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
-
-import java.time.Duration;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestApplication {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"))
-                .withMinimumRunningDuration(Duration.ofSeconds(3));
-    }
 
     public static void main(String[] args) {
         SpringApplication.from(Application::main).with(TestApplication.class).run(args);
