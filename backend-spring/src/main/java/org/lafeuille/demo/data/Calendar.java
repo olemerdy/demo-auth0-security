7ebf0d651f78ac64635f0c8a6affe7505be5e637
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-01-21 13:48:50 +0100

    Spotless

--- backend-spring/src/main/java/org/lafeuille/demo/data/Calendar.java
+++ backend-spring/src/main/java/org/lafeuille/demo/data/Calendar.java
@@ -2,14 +2,13 @@ package org.lafeuille.demo.data;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
-import org.springframework.data.jpa.domain.AbstractPersistable;
-
 import java.util.Collection;
 import java.util.UUID;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 public class Calendar extends AbstractPersistable<UUID> {
 
-    @OneToMany(mappedBy = "calendar")
-    private Collection<Event> events;
+  @OneToMany(mappedBy = "calendar")
+  private Collection<Event> events;
 }
--- backend-spring/src/main/java/org/lafeuille/demo/services/CalendarService.java
+++ backend-spring/src/main/java/org/lafeuille/demo/services/CalendarService.java
@@ -1,23 +1,22 @@
 package org.lafeuille.demo.services;
 
+import java.util.UUID;
 import org.lafeuille.demo.data.Event;
 import org.lafeuille.demo.data.EventRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class CalendarService {
 
-    private final EventRepository eventRepository;
+  private final EventRepository eventRepository;
 
-    public CalendarService(EventRepository eventRepository) {
-        this.eventRepository = eventRepository;
-    }
+  public CalendarService(EventRepository eventRepository) {
+    this.eventRepository = eventRepository;
+  }
 
-    public Page<Event> getCalendarEvents(UUID calendarId, Pageable pageable) {
-        return eventRepository.findAllByCalendarId(calendarId, pageable);
-    }
+  public Page<Event> getCalendarEvents(UUID calendarId, Pageable pageable) {
+    return eventRepository.findAllByCalendarId(calendarId, pageable);
+  }
 }
