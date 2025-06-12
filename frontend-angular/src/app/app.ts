1d831e9b8f274f21f97f863298f691ab70a630e3
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-06-12 15:32:20 +0200

    Putting back our code

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 import { RouterOutlet } from '@angular/router';
+import { YouTubePlayer } from '@angular/youtube-player';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GoogleMap, MatButtonModule, MatCardModule, RouterOutlet, YouTubePlayer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
-  protected title = 'frontend-angular';
+  title = 'frontend-angular';
 }
