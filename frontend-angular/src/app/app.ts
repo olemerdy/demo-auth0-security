ffed184aa86ab83f48165e3aae00aacd930230d8
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-02-09 09:14:59 +0100

    Angular update 21 (#88)

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,17 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { YouTubePlayer } from '@angular/youtube-player';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
+
 @Component({
   selector: 'app-root',
-  imports: [GoogleMap, MatButtonModule, MatCardModule, RouterOutlet, YouTubePlayer],
+  imports: [RouterOutlet, MatButtonModule, MatCardModule, RouterOutlet, YouTubePlayer],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrl: './app.css'
 })
 export class App {
-  title = 'frontend-angular';
+  protected readonly title = signal('frontend-angular');
 }
