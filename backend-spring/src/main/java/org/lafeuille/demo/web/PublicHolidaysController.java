9dc4849bafc1f4472583d81a9349833002f13bb9
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-02-11 13:48:51 +0100

    Filter for holiday types

--- backend-spring/src/main/java/org/lafeuille/demo/web/PublicHolidaysController.java
+++ backend-spring/src/main/java/org/lafeuille/demo/web/PublicHolidaysController.java
@@ -2,25 +2,24 @@ package org.lafeuille.demo.web;
 
 import de.focus_shift.jollyday.core.Holiday;
 import de.focus_shift.jollyday.core.HolidayManager;
+import de.focus_shift.jollyday.core.HolidayType;
 import de.focus_shift.jollyday.core.ManagerParameters;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Year;
 import java.util.Locale;
 import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 @RestController
 @RequestMapping("api/public/holidays")
 public class PublicHolidaysController {
 
     @GetMapping("{isoYear}")
-    public Set<Holiday> getYearHolidays(@PathVariable Year isoYear, Locale locale) {
+    public Set<Holiday> getYearHolidays(@PathVariable Year isoYear, Locale locale, @RequestParam(required = false) Set<HolidayType> types) {
         var holidayManager = HolidayManager.getInstance(ManagerParameters.create(locale));
         var holidays = holidayManager.getHolidays(isoYear);
-        System.out.println(holidays);
-        return holidays;
+        return holidays.stream().filter(holiday -> types == null || types.isEmpty() || types.contains(holiday.getType())).collect(toSet());
     }
 }
