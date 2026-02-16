eacc3b3867369b16d3a070639464fe5b4c3705d7
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-02-16 17:43:51 +0100

    Public Holiday

--- backend-spring/src/main/java/org/lafeuille/demo/web/PublicHolidaysController.java
+++ backend-spring/src/main/java/org/lafeuille/demo/web/PublicHolidaysController.java
@@ -4,22 +4,46 @@ import de.focus_shift.jollyday.core.Holiday;
 import de.focus_shift.jollyday.core.HolidayManager;
 import de.focus_shift.jollyday.core.HolidayType;
 import de.focus_shift.jollyday.core.ManagerParameters;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Year;
 import java.util.Locale;
 import java.util.Set;
 
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
 @RestController
 @RequestMapping("api/public/holidays")
 public class PublicHolidaysController {
 
-    @GetMapping("{isoYear}")
-    public Set<Holiday> getYearHolidays(@PathVariable Year isoYear, Locale locale, @RequestParam(required = false) Set<HolidayType> types) {
-        var holidayManager = HolidayManager.getInstance(ManagerParameters.create(locale));
-        var holidays = holidayManager.getHolidays(isoYear);
-        return holidays.stream().filter(holiday -> types == null || types.isEmpty() || types.contains(holiday.getType())).collect(toSet());
-    }
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<Holiday> getHolidays(@RequestParam LocalDate start, @RequestParam LocalDate end,
+      Locale locale, @RequestParam Optional<String> country) {
+    return getManager(locale, country)
+        .getHolidays(start, end)
+        .stream()
+        .sorted(Comparator.comparing(Holiday::getDate))
+        .collect(toList());
+  }
+
+  @GetMapping(path = "{isoYear}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<Holiday> getYearHolidays(@PathVariable Year isoYear, Locale locale,
+      @RequestParam Optional<String> country) {
+    return getManager(locale, country)
+        .getHolidays(isoYear)
+        .stream()
+        .sorted(Comparator.comparing(Holiday::getDate))
+        .collect(toList());
+  }
+
+  private HolidayManager getManager(Locale locale, Optional<String> country) {
+    var managerParameter = country.map(ManagerParameters::create).orElseGet(() -> ManagerParameters.create(locale));
+    return HolidayManager.getInstance(managerParameter);
+  }
 }
